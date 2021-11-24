2b69cc2fd69e67ba75d37d30e685074a9442792a
kishore0420 <51207213+kishore0420@users.noreply.github.com>
2021-11-24 12:48:01 +0530

    updated

--- index.js
+++ index.js
@@ -2,14 +2,8 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Thetips4you" }');
+    res.send('{ "response": "Hello everyone welcome to capgemini, this is a sample demo project" }');
 });
 
-app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
-});
-app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
-});
 app.listen(process.env.PORT || 3000);
 module.exports = app;
